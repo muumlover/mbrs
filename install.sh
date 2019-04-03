6f20edaa7b3ce5bbdf97f288d199526c9fbe41d0
muumlover <muumlover@live.com>
2019-04-03 19:40:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 apt update
-apt upgrade -y
 apt install zip python-pip libcurl4-openssl-dev libc6 -y
 pip install supervisor
+
+echo 'deb http://ftp.debian.org/debian/ buster main' >> /etc/apt/sources.list
+apt update
+apt -t buster install libc6
 
 mkdir mcbrs
 cd mcbrs
@@ -9,11 +12,12 @@ cd mcbrs
 wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.10.0.7.zip
 unzip bedrock-server-1.10.0.7.zip
 
-conf_path = "/etc/supervisor/conf.d/msc.conf"
+conf_path="/etc/supervisor/conf.d/msc.conf"
 touch $conf_path
 echo "[program:mcbrs]">>$conf_path
 echo "directory="$(pwd)>>$conf_path
-echo "command=LD_LIBRARY_PATH=. ./bedrock_server">>$conf_path
+echo "environment=LD_LIBRARY_PATH="$(pwd)>>$conf_path
+echo "command="$(pwd)"/bedrock_server">>$conf_path
 echo "autostart=true">>$conf_path
 echo "autorestart=true">>$conf_path
 echo "stderr_logfile=/tmp/mcbrs_stderr.log">>$conf_path
